7c53e4ecfda697ebce8855aca1fe6cc4767398fb
murilouwu <murilo.mendes11@etec.sp.gov.br>
2022-06-22 10:23:16 +0900

    update2

--- www/java.js
+++ www/java.js
@@ -1,7 +1,7 @@
+//função quando a tela carregar
 window.onload = ()=>{
     let div = document.querySelector('#select');
     let div2 = document.querySelector('#select2');
-    let bt = document.querySelector('#buscar');
     const opcoes = {
         method:'GET',
         mode:'cors',
@@ -11,13 +11,43 @@ window.onload = ()=>{
     .then(
         response => { response.json()
                 .then(data =>{
-                    /*for(let i; i<data.length; i++){
-                        div.innerHTML += "<option value='"+data[i]+"'>"+data[i]+"</option>";
-                        div2.innerHTML += "<option value='"+data[i]+"'>"+data[i]+"</option>";
-                        
-                        console.log("a");
-                    };*/
+                    let value = Object.keys(data);
+                    let texto = Object.values(data);
+                    for(i=0; i<texto.length; i++){
+                        div.innerHTML += "<option value='"+value[i]+"'>"+texto[i]+"</option>";
+                        div2.innerHTML += "<option value='"+value[i]+"'>"+texto[i]+"</option>";
+                    };
                 })
         }
     );
+};
+
+function resposta(){
+    let div = document.querySelector('#select');
+    let div2 = document.querySelector('#select2');
+    let cd = (div.value)+(div2.value);
+    let busca = document.querySelector('#busca');
+    let resposta = document.querySelector('#resposta');
+    busca.style.display = "none";
+    resposta.style.display = "flex";
+    fetch(`https://economia.awesomeapi.com.br/last/${div.value}-${div2.value}`,opcoes)
+    .then(
+        response => { response.json()
+            .then(data =>{
+                document.querySelector('#titulo').textContent = data[cd].name;
+                document.querySelector('#sp1').textContent = data[cd].bid;
+                document.querySelector('#sp2').textContent = data[cd].ask;
+                document.querySelector('#sp3').textContent = data[cd].varBid;
+                document.querySelector('#sp4').textContent = data[cd].high;
+                document.querySelector('#sp5').textContent = data[cd].low;
+            })
+        }
+    );
+};
+
+function busca(){
+    let busca = document.querySelector('#busca');
+    let resposta = document.querySelector('#resposta');
+    busca.style.display = "flex";
+    resposta.style.display = "none";
 };
