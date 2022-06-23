654ac42bdd0f6443216838700108dd7e21a0c44f
murilouwu <murilo.mendes11@etec.sp.gov.br>
2022-06-23 10:27:12 +0900

    :3

--- www/java.js
+++ www/java.js
@@ -1,12 +1,11 @@
-//função quando a tela carregar
 window.onload = ()=>{
     let div = document.querySelector('#select');
-    let div2 = document.querySelector('#select2');
+    let div2 = document.querySelector('#select2');    
     const opcoes = {
         method:'GET',
         mode:'cors',
         cache:'default'
-    }
+    }       
     fetch('https://economia.awesomeapi.com.br/json/available/uniq',opcoes)
     .then(
         response => { response.json()
@@ -14,40 +13,49 @@ window.onload = ()=>{
                     let value = Object.keys(data);
                     let texto = Object.values(data);
                     for(i=0; i<texto.length; i++){
-                        div.innerHTML += "<option value='"+value[i]+"'>"+texto[i]+"</option>";
-                        div2.innerHTML += "<option value='"+value[i]+"'>"+texto[i]+"</option>";
+                        div.innerHTML += "<option value='"+value[i]+"'>"+texto[i]+"</option>\n";
+                        div2.innerHTML += "<option value='"+value[i]+"'>"+texto[i]+"</option>\n";
                     };
                 })
         }
     );
-};
-
-function resposta(){
-    let div = document.querySelector('#select');
-    let div2 = document.querySelector('#select2');
-    let cd = (div.value)+(div2.value);
-    let busca = document.querySelector('#busca');
-    let resposta = document.querySelector('#resposta');
-    busca.style.display = "none";
-    resposta.style.display = "flex";
-    fetch(`https://economia.awesomeapi.com.br/last/${div.value}-${div2.value}`,opcoes)
-    .then(
-        response => { response.json()
-            .then(data =>{
-                document.querySelector('#titulo').textContent = data[cd].name;
-                document.querySelector('#sp1').textContent = data[cd].bid;
-                document.querySelector('#sp2').textContent = data[cd].ask;
-                document.querySelector('#sp3').textContent = data[cd].varBid;
-                document.querySelector('#sp4').textContent = data[cd].high;
-                document.querySelector('#sp5').textContent = data[cd].low;
-            })
-        }
-    );
-};
-
-function busca(){
-    let busca = document.querySelector('#busca');
-    let resposta = document.querySelector('#resposta');
-    busca.style.display = "flex";
-    resposta.style.display = "none";
+    let btn = document.querySelector('#buscar');
+    btn.addEventListener('click', ()=>{
+        let cd = (div.value)+(div2.value);
+        fetch(`https://economia.awesomeapi.com.br/json/available`,opcoes)
+        .then(
+            response => { response.json()
+                .then(data =>{
+                    if(data.hasOwnProperty(div.value+"-"+div2.value)){
+                        fetch(`https://economia.awesomeapi.com.br/last/${div.value}-${div2.value}`,opcoes)
+                        .then(
+                            response => { response.json()
+                                .then(data =>{
+                                    //titulo
+                                    document.querySelector('#titulo').innerHTML = data[cd].name;
+                                    
+                                    //compra
+                                    document.querySelector('#sp1').innerHTML = data[cd].ask;
+                                    
+                                    //venda
+                                    document.querySelector('#sp2').innerHTML = data[cd].bid;
+                                    
+                                    //variação
+                                    document.querySelector('#sp3').innerHTML = data[cd].pctChange;
+                                    
+                                    //maximo
+                                    document.querySelector('#sp4').innerHTML = data[cd].high;
+                                    
+                                    //minimo
+                                    document.querySelector('#sp1').innerHTML = data[cd].low;     
+                                })
+                            }
+                        );
+                    }else{
+                        document.querySelector('#titulo').innerHTML = "Não encontramos tal conversão na API";
+                    }
+                })
+            }
+        );
+    });
 };
